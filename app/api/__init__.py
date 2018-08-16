897cd8ca90475323ae3c42fb5e3dfd6043b50456
lustri <lustrigui@gmail.com>
2018-08-16 00:15:32 -0300

    Database postgres

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,9 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+DBUSER = 'lustri'
+DBPASS = 'lustri'
+DBHOST = '192.168.99.100'
+DBPORT = '5432'
+DBNAME = 'db'
  
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}'.format(
+        user=DBUSER,
+        passwd=DBPASS,
+        host=DBHOST,
+        port=DBPORT,
+        db=DBNAME)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'lustri'
+
 db = SQLAlchemy(app)
  
 from api.register.views import register
